ce23ba79ab59247c625d440c138eaae9efb5ced8
Hebert Silva <hfreitas86@gmail.com>
2023-04-20 15:40:58 -0300

    feat: add devdependecies and update template setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media',
+  mode: 'jit',
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
-}
+  plugins: [],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+/// <reference types="vitest" />
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
@@ -9,6 +10,6 @@ export default defineConfig({
     globals: true,
     environment: 'happy-dom',
     setupFiles: '.vitest/setup',
-    include: ['**/test.{ts,tsx}']
-  }
-})
+    include: ['**/*.spec.{ts,tsx}'],
+  },
+});
